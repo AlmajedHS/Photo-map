a13589bccc9c01df330e066fc5eb54ff891dc2d4
Hussain Almajed <almajedhs@Hussains-MacBook-Pro.local>
2018-02-15 01:21:56 -0500

    done with it

--- Photo Map/PhotoMapViewController.swift
+++ Photo Map/PhotoMapViewController.swift
@@ -7,13 +7,43 @@
 //
 
 import UIKit
+import MapKit
 
-class PhotoMapViewController: UIViewController {
 
+class PhotoMapViewController: UIViewController, MKMapViewDelegate, LocationsViewControllerDelegate {
+   
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let sfRegion = MKCoordinateRegionMake(CLLocationCoordinate2DMake(37.783333, -122.416667),
+                                              MKCoordinateSpanMake(0.1, 0.1))
+        mapView.setRegion(sfRegion, animated: false)
+        
+        mapView.delegate = self
+        
+        
+        
 
         // Do any additional setup after loading the view.
+    }
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let reuseID = "myAnnotationView"
+        
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseID)
+        if (annotationView == nil) {
+            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseID)
+            annotationView!.canShowCallout = true
+            annotationView!.leftCalloutAccessoryView = UIImageView(frame: CGRect(x:0, y:0, width: 50, height:50))
+        }
+        
+        let imageView = annotationView?.leftCalloutAccessoryView as! UIImageView
+        imageView.image = UIImage(named: "camera")
+        
+        return annotationView
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,6 +52,9 @@ class PhotoMapViewController: UIViewController {
     }
     
 
+    @IBAction func onPhoto(_ sender: Any) {
+        instantiateImagePickerController()
+    }
     
     // MARK: - Navigation
 
@@ -29,7 +62,58 @@ class PhotoMapViewController: UIViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        let locationsViewController = segue.destination as! LocationsViewController
+        locationsViewController.delegate = self
+        
     }
     
+    func locationsPickedLocation(controller: LocationsViewController, latitude: NSNumber, longitude: NSNumber) {
+        print("hello")
+        controller.navigationController?.popViewController(animated: true)
+        
+        let locationCoordinate = CLLocationCoordinate2D(latitude: CLLocationDegrees(latitude), longitude: CLLocationDegrees(longitude))
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = locationCoordinate
+        annotation.title = "Picture!"
+        mapView.addAnnotation(annotation)
+    }
+    
+    
 
+}
+extension PhotoMapViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    public func instantiateImagePickerController() {
+        let vc = UIImagePickerController()
+        vc.delegate = self
+        vc.allowsEditing = true
+        vc.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        
+        //        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+        //            print("Camera is available 📸")
+        //            vc.sourceType = .camera
+        //        } else {
+        //            print("Camera 🚫 available so we will use photo library instead")
+        //            vc.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        //        }
+        //
+        
+        self.present(vc, animated: true, completion: nil)
+    }
+    
+    public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        // Get the image captured by the UIImagePickerController
+        let originalImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        let editedImage = info[UIImagePickerControllerEditedImage] as! UIImage
+        
+        // Do something with the images (based on your use case)
+        //postImView.image = editedImage
+        
+       
+        picker.dismiss(animated: true, completion: nil)
+          self.performSegue(withIdentifier: "tagSegue", sender: nil)
+        
+    
+    }
+    
+    
 }
